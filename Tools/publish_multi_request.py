8d6cb9794c871985a7ac4442a02cfb0022e3fc6d
tfmb <103616394+thefuckmybrain@users.noreply.github.com>
2024-09-22 17:31:46 +0300

    Update publish.yml
    fixed conflicts

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,8 +14,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://wizards.cdn.spacestation14.com/"
-FORK_ID = "wizards"
+ROBUST_CDN_URL = "https://build.vanilla-station.ru/"
+FORK_ID = "Vanilla_Station"
 
 def main():
     session = requests.Session()
